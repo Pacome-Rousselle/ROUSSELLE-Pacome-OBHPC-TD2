84529bee00a2c426fe96383b9cded9c82f5f984d
Pacome Rousselle <pacomerouselle@gmail.com>
2022-11-09 22:49:12 +0100

    added taskset commands

--- reduc/run_reduc.sh
+++ reduc/run_reduc.sh
@@ -1,44 +1,45 @@
 #!/bin/bash
 
 data_size="100 50"
+task="taskset -c 1"
 make clean
 
 make CC=gcc
 #Question 1, no optflags
-./reduc $data_size > gccO0.dat
+$task ./reduc $data_size > gccO0.dat
 #Question 2-3, optflags
-./reducO1 $data_size > gccO1.dat
-./reducO2 $data_size > gccO2.dat
-./reducO3 $data_size > gccO3.dat
-./reducOfast $data_size > gccOfast.dat
+$task ./reducO1 $data_size > gccO1.dat
+$task ./reducO2 $data_size > gccO2.dat
+$task ./reducO3 $data_size > gccO3.dat
+$task ./reducOfast $data_size > gccOfast.dat
 make clean
 
 make CC=clang
 #No optflags
-./reduc $data_size > clangO0.dat
+$task ./reduc $data_size > clangO0.dat
 #Optflags
-./reducO1 $data_size > clangO1.dat
-./reducO2 $data_size > clangO2.dat
-./reducO3 $data_size > clangO3.dat
-./reducOfast  $data_size > clangOfast.dat
+$task ./reducO1 $data_size > clangO1.dat
+$task ./reducO2 $data_size > clangO2.dat
+$task ./reducO3 $data_size > clangO3.dat
+$task ./reducOfast  $data_size > clangOfast.dat
 make clean
 
 # make CC=icc
 #No optflags
-# ./reduc $data_size > iccO0.dat
+# $task ./reduc $data_size > iccO0.dat
 #Optflags
-# ./reducO1 $data_size > iccO1.dat
-# ./reducO2 $data_size > iccO2.dat
-# ./reducO3 $data_size > iccO3.dat
-# ./reducOfast $data_size > iccOfast.dat
+# $task ./reducO1 $data_size > iccO1.dat
+# $task ./reducO2 $data_size > iccO2.dat
+# $task ./reducO3 $data_size > iccO3.dat
+# $task ./reducOfast $data_size > iccOfast.dat
 # make clean
 
 # make CC=icx
 #No optflags
-# ./reduc $data_size > icxO0.dat
+# $task ./reduc $data_size > icxO0.dat
 #Optflags
-# ./reducO1 $data_size > icxO1.dat
-# ./reducO2 $data_size > icxO2.dat
-# ./reducO3 $data_size > icxO3.dat
-# ./reducOfast $data_size > icxOfast.dat
+# $task ./reducO1 $data_size > icxO1.dat
+# $task ./reducO2 $data_size > icxO2.dat
+# $task ./reducO3 $data_size > icxO3.dat
+# $task ./reducOfast $data_size > icxOfast.dat
 # make clean
