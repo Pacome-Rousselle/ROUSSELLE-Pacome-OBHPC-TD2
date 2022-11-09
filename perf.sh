0b72d3fb3cba93372d4a9d277c25ae44b1944ba6
Pacome Rousselle <pacomerouselle@gmail.com>
2022-11-09 22:49:56 +0100

    added cpupower commands

--- perf.sh
+++ perf.sh
@@ -1,16 +1,17 @@
-# sudo cpupower -c 0 frequency-set -g performance
-
 cd dgemm
+sudo cpupower -c 1 frequency-set -g performance
 ./run_dgemm.sh
 ./data_dgemm.sh
 cd ..
 
 cd dotprod
+sudo cpupower -c 1 frequency-set -g performance
 ./run_dotprod.sh
 ./data_dotprod.sh
 cd ..
 
 cd reduc
+sudo cpupower -c 1 frequency-set -g performance
 ./run_reduc.sh
 ./data_reduc.sh
 cd ..
