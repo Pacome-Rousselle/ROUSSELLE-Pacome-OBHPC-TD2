e75c9696f65020b5975bd52f592c6629bfa9a334
Pacome Rousselle <pacomerouselle@gmail.com>
2022-11-09 15:27:49 +0100

    Adapted to dotprod
    Added plotting section with gnuplot
    Adaptated the script to dotprod outputs

--- dotprod/data_dotprod.sh
+++ dotprod/data_dotprod.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 
-directory="datafiles"
-rm -rf $directory
-mkdir $directory
+ver="versions"
+CC="compilers"
+img="graphs"
+
+rm -rf $ver $CC $img
+mkdir $ver $CC $img
 
 for file in $(ls *.dat); do
-  grep "BASE" "$file" >> "$directory/base.dat" 
-  sed -i "s/BASE/${file%%.dat}/g" "$directory/base.dat"
-  
-  grep "UNROLL4" "$file" >> "$directory/unroll4.dat" 
-  sed -i "s/UNROLL4/${file%%.dat}/g" "$directory/unroll4.dat"
+  grep "BASE" "$file" >> "$ver/base.dat" 
+  sed -i "s/BASE/${file%%.dat}/g" "$ver/base.dat"
 
-  grep "UNROLL8" "$file" >> "$directory/unroll8.dat" 
-  sed -i "s/UNROLL8/${file%%.dat}/g" "$directory/unroll8.dat"
+  grep "UNROLL8" "$file" >> "$ver/unroll8.dat" 
+  sed -i "s/UNROLL8/${file%%.dat}/g" "$ver/unroll8.dat"
 
-  grep "CBLAS" "$file" >> "$directory/cblas.dat" 
-  sed -i "s/CBLAS/${file%%.dat}/g" "$directory/cblas.dat"
+  grep "CBLAS" "$file" >> "$ver/cblas.dat" 
+  sed -i "s/CBLAS/${file%%.dat}/g" "$ver/cblas.dat"
 
-  rm $file
+  tail -n +2 "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+  mv $file $CC
 done 
+
+#GNUPLOT
+cp versions.gp $ver; cp versions_all.gp $ver
+cp gcc.gp $CC; cp clang.gp $CC
+cp gcc_all.gp $CC; cp clang_all.gp $CC
+
+cd $ver; gnuplot versions.gp; gnuplot versions_all.gp
+mv *.png ..
+rm -f *.gp 
+cd ..
+
+cd $CC
+gnuplot gcc.gp; gnuplot clang.gp; 
+gnuplot gcc_all.gp; gnuplot clang_all.gp; 
+
+mv *.png ..
+rm -f *.gp
+cd ..
+
+mv *.png $img
