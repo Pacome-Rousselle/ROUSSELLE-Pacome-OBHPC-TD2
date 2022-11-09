81f80ebcac64af05f81b13e79b590763f6c5cede
Pacome Rousselle <pacomerouselle@gmail.com>
2022-11-09 15:31:06 +0100

    added taskset commands

--- dotprod/run_dotprod.sh
+++ dotprod/run_dotprod.sh
@@ -1,44 +1,45 @@
 #!/bin/bash
 
 data_size="100 50"
+task="taskset -c 1"
 make clean
 
 make CC=gcc
 #Question 1, no optflags
-./dotprod $data_size > gccO0.dat
+$task ./dotprod $data_size > gccO0.dat
 #Question 2-3, optflags
-./dotprodO1 $data_size > gccO1.dat
-./dotprodO2 $data_size > gccO2.dat
-./dotprodO3 $data_size > gccO3.dat
-./dotprodOfast $data_size > gccOfast.dat
+$task ./dotprodO1 $data_size > gccO1.dat
+$task ./dotprodO2 $data_size > gccO2.dat
+$task ./dotprodO3 $data_size > gccO3.dat
+$task ./dotprodOfast $data_size > gccOfast.dat
 make clean
 
 make CC=clang
 #No optflags
-./dotprod $data_size > clangO0.dat
+$task ./dotprod $data_size > clangO0.dat
 #Optflags
-./dotprodO1 $data_size > clangO1.dat
-./dotprodO2 $data_size > clangO2.dat
-./dotprodO3 $data_size > clangO3.dat
-./dotprodOfast  $data_size > clangOfast.dat
+$task ./dotprodO1 $data_size > clangO1.dat
+$task ./dotprodO2 $data_size > clangO2.dat
+$task ./dotprodO3 $data_size > clangO3.dat
+$task ./dotprodOfast  $data_size > clangOfast.dat
 make clean
 
 # make CC=icc
 #No optflags
-# ./dotprod $data_size > iccO0.dat
+# $task ./dotprod $data_size > iccO0.dat
 #Optflags
-# ./dotprodO1 $data_size > iccO1.dat
-# ./dotprodO2 $data_size > iccO2.dat
-# ./dotprodO3 $data_size > iccO3.dat
-# ./dotprodOfast $data_size > iccOfast.dat
+# $task ./dotprodO1 $data_size > iccO1.dat
+# $task ./dotprodO2 $data_size > iccO2.dat
+# $task ./dotprodO3 $data_size > iccO3.dat
+# $task ./dotprodOfast $data_size > iccOfast.dat
 # make clean
 
 # make CC=icx
 #No optflags
-# ./dotprod $data_size > icxO0.dat
+# $task ./dotprod $data_size > icxO0.dat
 #Optflags
-# ./dotprodO1 $data_size > icxO1.dat
-# ./dotprodO2 $data_size > icxO2.dat
-# ./dotprodO3 $data_size > icxO3.dat
-# ./dotprodOfast $data_size > icxOfast.dat
+# $task ./dotprodO1 $data_size > icxO1.dat
+# $task ./dotprodO2 $data_size > icxO2.dat
+# $task ./dotprodO3 $data_size > icxO3.dat
+# $task ./dotprodOfast $data_size > icxOfast.dat
 # make clean
