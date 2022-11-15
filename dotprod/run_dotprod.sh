ebe62d796a0a7a0d4a95a625c4507ce2adb92db6
Pacome Rousselle <pacomerouselle@gmail.com>
2022-11-15 11:35:07 +0100

    Separated data size and kernel repetitions variables in run scripts

--- dotprod/run_dotprod.sh
+++ dotprod/run_dotprod.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 
-data_size="100 50"
+data_size="100"
+kernel="50"
 task="taskset -c 1"
 make clean
 
 make CC=gcc
 #Question 1, no optflags
-$task ./dotprod $data_size > gccO0.dat
+$task ./dotprod $data_size $kernel > gccO0.dat
 #Question 2-3, optflags
-$task ./dotprodO1 $data_size > gccO1.dat
-$task ./dotprodO2 $data_size > gccO2.dat
-$task ./dotprodO3 $data_size > gccO3.dat
-$task ./dotprodOfast $data_size > gccOfast.dat
+$task ./dotprodO1 $data_size $kernel > gccO1.dat
+$task ./dotprodO2 $data_size $kernel > gccO2.dat
+$task ./dotprodO3 $data_size $kernel > gccO3.dat
+$task ./dotprodOfast $data_size $kernel > gccOfast.dat
 make clean
 
 make CC=clang
 #No optflags
-$task ./dotprod $data_size > clangO0.dat
+$task ./dotprod $data_size $kernel > clangO0.dat
 #Optflags
-$task ./dotprodO1 $data_size > clangO1.dat
-$task ./dotprodO2 $data_size > clangO2.dat
-$task ./dotprodO3 $data_size > clangO3.dat
-$task ./dotprodOfast  $data_size > clangOfast.dat
+$task ./dotprodO1 $data_size $kernel > clangO1.dat
+$task ./dotprodO2 $data_size $kernel > clangO2.dat
+$task ./dotprodO3 $data_size $kernel > clangO3.dat
+$task ./dotprodOfast  $data_size $kernel > clangOfast.dat
 make clean
 
 # make CC=icc
 #No optflags
-# $task ./dotprod $data_size > iccO0.dat
+# $task ./dotprod $data_size $kernel > iccO0.dat
 #Optflags
-# $task ./dotprodO1 $data_size > iccO1.dat
-# $task ./dotprodO2 $data_size > iccO2.dat
-# $task ./dotprodO3 $data_size > iccO3.dat
-# $task ./dotprodOfast $data_size > iccOfast.dat
+# $task ./dotprodO1 $data_size $kernel > iccO1.dat
+# $task ./dotprodO2 $data_size $kernel > iccO2.dat
+# $task ./dotprodO3 $data_size $kernel > iccO3.dat
+# $task ./dotprodOfast $data_size $kernel > iccOfast.dat
 # make clean
 
 # make CC=icx
 #No optflags
-# $task ./dotprod $data_size > icxO0.dat
+# $task ./dotprod $data_size $kernel > icxO0.dat
 #Optflags
-# $task ./dotprodO1 $data_size > icxO1.dat
-# $task ./dotprodO2 $data_size > icxO2.dat
-# $task ./dotprodO3 $data_size > icxO3.dat
-# $task ./dotprodOfast $data_size > icxOfast.dat
+# $task ./dotprodO1 $data_size $kernel > icxO1.dat
+# $task ./dotprodO2 $data_size $kernel > icxO2.dat
+# $task ./dotprodO3 $data_size $kernel > icxO3.dat
+# $task ./dotprodOfast $data_size $kernel > icxOfast.dat
 # make clean
